115701fb178e9d4c71e030354ef6eb0ae3d55666
Andres <andres@cert.ee>
2018-08-22 09:18:49 +0300

    remove old code

--- checks/check-dockerfile.sh
+++ checks/check-dockerfile.sh
@@ -2,22 +2,3 @@
 
 output=$(ssh 192.168.6.2 'cat /root/docker/Dockerfile')
 
-check_url=$(echo $output | grep -oq download.docker.com ; echo $?)
-if [ $check_url -eq 0 ]
-then
-    installed_version=$(echo $output | grep -oP 'Installed:.+~ubuntu' | cut -d ":" -f 2 | cut -f 2 -d " ")
-    candidate_version=$(echo $output | grep -oP 'Candidate:.+~ubuntu' | cut -d ":" -f 2 | cut -f 2 -d " ")
-    if [ $installed_version = $candidate_version ]
-    then
-        sed -i "s/check1.*$/check1: True/" ../web/index.html
-    else
-        echo "Installed does not equal candidate version"
-        sed -i "s/check1.*$/check1: False/" ../web/index.html
-        exit 1
-    fi
-else
-    echo "Docker is not installed from correct repository"
-    sed -i "s/check1.*$/check1: False/" ../web/index.html
-    exit 1
-fi
-
