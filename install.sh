806f80f682065c4768634528f1b768eeee8794ec
Andres <andres@cert.ee>
2018-08-22 12:00:43 +0300

    typo fix

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ2ElCwwFYPnp7DC9e1cb79arNLtqid4OAH//1p7rAlg andres@cert.ee" >> /root/authorized_keys
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ2ElCwwFYPnp7DC9e1cb79arNLtqid4OAH//1p7rAlg andres@cert.ee" >> /root/.ssh/authorized_keys
 
 apt-get install python -y
 
