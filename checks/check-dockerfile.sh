3388670e3be1d8303eb0774b147f3ac7b86fa45e
Rasmus Koit <rasmuskoit@gmail.com>
2018-08-22 12:29:56 +0300

    Change check info on website

--- checks/check-dockerfile.sh
+++ checks/check-dockerfile.sh
@@ -4,13 +4,14 @@ FILE_PATH="/home/student/Dockerfile"
 if ssh $HOST "[ -f $FILE_PATH ]"
 then
     echo "File exists"
-    # sed -i "s/check2.*$/check2: True/" ../web/index.html
     OUTPUT=$(ssh $HOST cat $FILE_PATH)
     if echo $OUTPUT | grep "FROM" | grep "MAINTAINER" | grep "RUN apt" | grep "update" | grep "nginx" -oq
     then
-        echo "6ige"
+        echo "Dockerfile seems valid"
+        sed -i "s/check2.*$/check2: True/" ../web/index.html
     else
-        echo "v2hem 6ige"
+        echo "Dockerfile is missing some info"
+        sed -i "s/check2.*$/check2: False/" ../web/index.html
         exit 1
     fi
 else
