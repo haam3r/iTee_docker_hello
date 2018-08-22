605b7e6ba70e6af6a1e8f0fd8553940e67967247
Rasmus Koit <rasmuskoit@gmail.com>
2018-08-22 11:22:06 +0300

    check for file validation

--- checks/check-dockerfile.sh
+++ checks/check-dockerfile.sh
@@ -5,7 +5,14 @@ if ssh $HOST "[ -f $FILE_PATH ]"
 then
     echo "File exists"
     # sed -i "s/check2.*$/check2: True/" ../web/index.html
-    
+    OUTPUT=$(ssh $HOST cat $FILE_PATH)
+    if echo $OUTPUT | grep "FROM" | grep "MAINTAINER" | grep "RUN apt" | grep "update" | grep "nginx" -oq
+    then
+        echo "6ige"
+    else
+        echo "v2hem 6ige"
+        exit 1
+    fi
 else
     echo "File does not exist"
     sed -i "s/check2.*$/check2: False/" ../web/index.html
