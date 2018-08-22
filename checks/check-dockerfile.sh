21ca609a26e6fa0bd676309f65473bd2733997ed
Rasmus Koit <rasmuskoit@gmail.com>
2018-08-22 10:03:46 +0300

    Check if file exists

--- checks/check-dockerfile.sh
+++ checks/check-dockerfile.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-
-output=$(ssh 192.168.6.2 'cat /root/docker/Dockerfile')
-
+HOST="192.168.6.2"
+FILE_PATH="/home/student/Dockerfile"
+if ssh $HOST stat $FILE_PATH \> /dev/null 2\>\&1
+then
+    echo "File exists"
+else
+    echo "File does not exist"
+fi
